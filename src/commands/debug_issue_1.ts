78bf355b67a968094278441957537a745e5f7e8c
Vlad Frangu <kingdgrizzle@gmail.com>
2022-02-20 03:59:18 +0200

    chore: debugging 101

---
+++ src/commands/debug_issue_1.ts
@@ -0,0 +1,22 @@
+import { ApplicationCommandRegistry, Command } from '@sapphire/framework';
+import type { CommandInteraction } from 'discord.js';
+
+export class DebugIssue1ByHenning extends Command {
+	public override async chatInputRun(interaction: CommandInteraction) {
+		return interaction.reply({ ephemeral: true, content: "This maze wasn't meant for you." });
+	}
+
+	public override registerApplicationCommands(registry: ApplicationCommandRegistry) {
+		registry.registerChatInputCommand(
+			(builder) =>
+				builder
+					.setName('debug_issue_1')
+					.setDescription('Debug issue 1; reported by Henning')
+					.addIntegerOption((owoLevel) => owoLevel.setName('owo-level').setDescription('How owo of you').setMinValue(69).setMaxValue(420)),
+			{
+				guildIds: ['737141877803057244'],
+				idHints: ['944771600379691098']
+			}
+		);
+	}
+}
